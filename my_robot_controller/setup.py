5c36f8c56eecdd87e27f893c512606946c981e76
ahmeth <ahmethaathil@gmail.com>
2025-01-19 23:23:44 +0530

    n

--- my_robot_controller/setup.py
+++ my_robot_controller/setup.py
@@ -14,13 +14,15 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='aathil',
-    maintainer_email='aathil@todo.todo',
+    maintainer_email='aathilahmeth@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "test_node = my_robot_controller.my_first_node: main "
+            "test_node = my_robot_controller.my_first_node: main"
+            "draw_circle = my_robot_controller.draw_circle: main"
+            "pose_subscriber = my_robot_controller.pose_subscriber: main"
         ],
     },
 )
