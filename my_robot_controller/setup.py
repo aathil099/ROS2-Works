b944ddc1d15fe56d527bc574e7638adad3785632
ahmeth <ahmethaathil@gmail.com>
2025-01-20 15:10:44 +0530

    wd

--- my_robot_controller/setup.py
+++ my_robot_controller/setup.py
@@ -23,7 +23,8 @@ setup(
             "test_node = my_robot_controller.my_first_node: main",
             "Draw_circle = my_robot_controller.Draw_circle: main",
             "draw_circle = my_robot_controller.draw_circle: main",
-            "pose_subscriber = my_robot_controller.pose_subscriber: main"
+            "pose_subscriber = my_robot_controller.pose_subscriber: main",
+            "turtle_controller = my_robot_controller.turtle_controller:main"
         ],
     },
 )
